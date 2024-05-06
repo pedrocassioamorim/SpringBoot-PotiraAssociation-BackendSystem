84b5f7efcab7bd62ed83eac75079d3763270565b
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-05-06 00:05:03 -0300

    Povoando DB

--- src/main/java/com/example/associacao_potira/domain/people/Employee.java
+++ src/main/java/com/example/associacao_potira/domain/people/Employee.java
@@ -5,15 +5,24 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.time.LocalDate;
+
 @Data @Entity @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Employee {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @EqualsAndHashCode.Include
     private Long id;
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
+
+    private String cpf;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
 
     private String email;
 
@@ -21,8 +30,12 @@ public class Employee {
 
     private String address;
 
+    private Double salary;
+
     @Enumerated(EnumType.STRING)
     private Department department;
+
+
 
 
 
