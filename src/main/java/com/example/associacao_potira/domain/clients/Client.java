b68d81ef533f770eab2dff21105aa3229d8cdb01
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-06-12 17:15:27 -0300

    Improving more serial packages

--- src/main/java/com/example/associacao_potira/domain/clients/Client.java
+++ src/main/java/com/example/associacao_potira/domain/clients/Client.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.proxy.HibernateProxy;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Objects;
 
 @Getter
@@ -11,7 +13,10 @@ import java.util.Objects;
 @ToString
 @RequiredArgsConstructor
 @Entity
-public class Client {
+public class Client implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
